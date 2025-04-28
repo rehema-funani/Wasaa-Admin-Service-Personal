a414b71da9494359dc46278471ddd55006e31400
salvatoluice <salvatoluice5@gmail.com>
2025-04-28 08:48:26 +0300

    -ft: initial commit and file set up

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <p>Welcome to your finance-integrated communication platform</p>
+    </div>
+  );
+}
---
+++ src/constants/routes.js
@@ -0,0 +1,32 @@
+import { 
+  IoHome, 
+  IoGlobe, 
+  IoPeople, 
+  IoTvOutline,
+  IoTimeOutline,
+  IoVideocamOutline,
+  IoBarChartOutline,
+  IoShieldOutline,
+  IoReceiptOutline,
+  IoWalletOutline,
+  IoCardOutline,
+  IoStatsChartOutline,
+  IoStarOutline,
+  IoWarning,
+  IoImages,
+  IoColorPalette,
+  IoSettingsSharp
+} from 'react-icons/io5';
+import { Users } from 'lucide-react';
+
+const routes = [
+  {
+    type: 'link',
+    path: '/',
+    title: 'Dashboard',
+    icon: IoHome,
+  },
+  // rest of the routes...
+];
+
+export default routes;
